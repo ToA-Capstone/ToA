59c7d014c5f70fd6afee885abb40751852784978
minhwan <minhwanpark@naver.com>
2023-06-11 22:34:11 +0900

    #29 아이콘 마무리

--- app/src/main/java/capstone/app/toa/MainActivity.java
+++ app/src/main/java/capstone/app/toa/MainActivity.java
@@ -27,12 +27,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!api.getUserManager().isLogin()) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-        }
+//        if (!api.getUserManager().isLogin()) {
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(intent);
+//            finish();
+//        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
